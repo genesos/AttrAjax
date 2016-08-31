4e1a3d2f416c60203e713c2b0492ce80fc37e28f
blu <blu@ridi.com>
2016-08-31 11:38:41 +0900

    fix dependencies

--- jquery.attrajax.js
+++ jquery.attrajax.js
@@ -6,7 +6,7 @@
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     // using AMD; register as anon module
-    define(['jquery', 'jquery.form'], factory);
+    define(['jquery', 'jquery-form'], factory);
   } else {
     // no AMD; invoke directly
     factory((typeof(jQuery) !== 'undefined') ? jQuery : window.Zepto);
